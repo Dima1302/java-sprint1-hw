ec4b32ac4d0339f884b3f9c110d007482239d7de
Dima1302 <102532586+Dima1302@users.noreply.github.com>
2022-04-03 20:23:08 +0900

    Delete MonthData.java

--- src/MonthData.java
+++
@@ -1,8 +0,0 @@
-public class MonthData {
-    int[] array = new int[30];// Заполните класс самостоятельно
-    public MonthData() {
-        for (int i = 0; i < array.length; i++) {
-            array[i] = 0;
-        }
-    }
-}
